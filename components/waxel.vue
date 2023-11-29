5493b307fd7c47fc9f973f7458344f39154be19e
Wapaca <wapaca@gmail.com>
2023-11-29 22:58:44 +0100

    Display list of actors and their ninja + add current connected wallet to actors

--- components/waxel.vue
+++ components/waxel.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div class="waxel">
 		<ClientOnly>
 			<div>
 				<div v-if="chainStore.session">
@@ -17,17 +17,28 @@
 			<hr>
 			<hr>
 
-			<div class="ninjas-list">
-				<div v-for="ninja in waxelStore.ninjasList" class="ninjas-list-item">
-					<div>{{ ninja.asset_id }}</div>
-					<div>{{ ninja.delay_seconds }}</div>
-					<div>{{ ninja.last_search }}</div>
-					<div>{{ ninja.owner }}</div>
-					<div>{{ ninja.race }}</div>
-					<div>{{ ninja.status }}</div>
-					<hr>
+			<div class="waxel-actors">
+				<div class="waxel-actors-list">
+					<div v-for="actor in waxelStore.actors" class="waxel-actors-list-item">
+						<div class="item-label">{{ actor }}</div>
+						<div v-if="waxelStore.ninjasList[actor] !== undefined" class="item-ninjas-list">
+							<div v-for="ninja in waxelStore.ninjasList[actor]" class="ninjas-list-item">
+								<div>{{ ninja.asset_id }}</div>
+								<div>{{ ninja.delay_seconds }}</div>
+								<div>{{ ninja.last_search }}</div>
+								<div>{{ ninja.owner }}</div>
+								<div>{{ ninja.race }}</div>
+								<div>{{ ninja.status }}</div>
+								<hr>
+							</div>
+						</div>
+						<div v-else>
+							<span>Go on <a href="https://play.waxel.net/ww/index.html" target="_blank">Waxel game</a> to stake your ninjas</span>
+						</div>
+					</div>
 				</div>
 			</div>
+
 		</ClientOnly>
 	</div>
 </template>
--- stores/waxel.js
+++ stores/waxel.js
@@ -7,7 +7,7 @@ export const useWaxelStore = defineStore('waxel', {
   state: () => ({
     actors: [],
     ninjas: [],
-    ninjasList: []
+    ninjasList: {} // ninjasList sorted by actors
   }),
   actions: {
     async init() {
@@ -15,11 +15,30 @@ export const useWaxelStore = defineStore('waxel', {
       this.initNinjas()
     },
     initActors() {
+      this.actors = []
+      if(process.client) {
+        const jsonActors = localStorage.getItem("waxelStore-actors");
+        if(jsonActors)
+          this.actors = JSON.parse(jsonActors)
+      }
+
+      this.addConnectedActor()
+    },
+    addConnectedActor() {
       const chainStore = useChainStore()
 
       if(chainStore.session) {
-        this.actors.push(chainStore.session.actor.toString())
+        const connected = chainStore.session.actor.toString()
+
+        if(!this.actors.includes(connected))
+          this.actors.push(connected)
       }
+
+      this.saveActors()
+    },
+    saveActors() {
+      if(process.client)
+        localStorage.setItem("waxelStore-actors", JSON.stringify(this.actors))
     },
     async initNinjas() {
       const ninjasFetch = await fetchTable('waxelworldsc', 'waxelworldsc', 'ninjas', true);
@@ -28,7 +47,26 @@ export const useWaxelStore = defineStore('waxel', {
 
         return n;
       })
-      this.ninjasList = this.ninjas.filter(n => this.actors.includes(n.owner))
+
+      /*** Different ninjas status 
+        0: "Searching"
+        1: "Search successful"
+        2: "holdup1"
+        3: "Search failed"
+        4: "Idle"
+        5: "holdup"
+      **/
+
+      this.updateNinjasList()
+    },
+    updateNinjasList() {
+      this.ninjasList = {}
+      for(const ninja of this.ninjas) {
+        if(this.ninjasList[ninja.owner] === undefined)
+          this.ninjasList[ninja.owner] = []
+
+        this.ninjasList[ninja.owner].push(ninja)
+      }
     }
   },
   getters: {
